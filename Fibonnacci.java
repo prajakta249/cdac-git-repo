bd86615672ac862861705bb5646342e5f717dd76
prajakta249 <97919410+prajakta249@users.noreply.github.com>
2022-01-22 15:56:46 +0530

    Create Fibonnacci.java
    Fibonnacci Series

---
+++ Fibonnacci.java
@@ -0,0 +1,15 @@
+class Fibonnacci
+{
+  public static void main(String[] args)
+  {
+    int n=10, f1=0, s1=1;
+    System.out.println("Fibonnacci Series till " +n+ " terms: " );
+    for(int i=1;i<=n;i++)
+    {
+      System.out.println(f1 + ", ");
+      int nextTerm = f1+s1;
+      f1=s1;
+      s1=nextTerm;
+    }
+  }
+}
